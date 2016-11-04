ffebf7235a1911d127b9467e6f197fc6c60d9336
calebcox725 <caleb.cox@flatironschool.com>
2016-11-04 16:36:03 -0400

    Done.

--- js/events.js
+++ js/events.js
@@ -1,4 +1,29 @@
 //define functions here
+function getIt() {
+  $('p').on('click', () => {
+    alert('Hey!');
+  });
+} ;
+
+function frameIt() {
+  $('img').on('load', () => {
+    $('img').addClass('tasty');
+  });
+};
+
+function submitIt() {
+  $('form').on('submit', () => {
+    alert("Your form is going to be submitted now.");
+  });
+};
+
+function pressIt() {
+  $('input').on('keydown', (e) => {
+    if (e.which === 71) {
+      alert("G");
+    };
+  });
+};
 
 $(document).ready(function(){
 
